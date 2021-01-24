3ae473697178e568eec4c9a5633acbe1bf6869a3
1024 <54119958+1024HQ@users.noreply.github.com>
2021-01-24 14:09:27 +0700

    import key by $

--- bitcoin-bitkub.php
+++ bitcoin-bitkub.php
@@ -13,11 +13,11 @@ $content = file_get_contents("https://api.bitkub.com/api/market/ticker?sym=USD_B
 $obj = json_decode($content); //decode json data
 // USD-THB API
 if($usd_server == 0){
-		$content2 = file_get_contents("http://api.currencylayer.com/live?access_key=5d56876a070feef0811c6fd252e714dd&format=1&currencies=THB");
+		$content2 = file_get_contents("http://api.currencylayer.com/live?access_key=".$key1."&format=1&currencies=THB");
 		$obj2 = json_decode($content2);
 		$usd = $obj2->quotes->USDTHB; // USD:THB ดึงข้อมูล หัวข้อ quotes ฟิลด์ USDTHB
 }else if($usd_server == 1){
-		$content2 = file_get_contents("https://free.currconv.com/api/v7/convert?q=USD_THB&compact=ultra&apiKey=2d9c96bce0dc65d4db62");
+		$content2 = file_get_contents("https://free.currconv.com/api/v7/convert?q=USD_THB&compact=ultra&apiKey=".$key2."");
 		$obj2 = json_decode($content2);
 		$usd = $obj2->USD_THB; // USD:THB อันนี้ API เป็นแบบชั้นเดียวง่าย ๆ เลยดึงจากชื่อมาได้เลย USD_THB
 }
